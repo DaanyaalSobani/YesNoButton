9c66ad5c19f4172af8a8d79824d2af569db4c40a
Daanyaal Sobani <daanyaal.sobani@gmail.com>
2023-06-30 19:03:43 -0400

    Initial Version

---
+++ script.js
@@ -0,0 +1,9 @@
+function yesFunction() {
+    // Define your function here
+    console.log("Yes button pressed");
+}
+
+function noFunction() {
+    // Define your function here
+    console.log("No button pressed");
+}
